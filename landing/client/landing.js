080e5e292925013852bceca1b21d532e934bb0b5
Karl Danninger <kdanninger@gmail.com>
2015-10-10 18:47:05 -0400

    add login buttons to dashboard. add layout that handles state

--- landing/client/landing.js
+++ landing/client/landing.js
@@ -2,6 +2,5 @@ Template.landing.events({
 	'click .github-signup': function(evt, temp) {
 		evt.preventDefault();
 		Meteor.loginWithGithub();
-		FlowRouter.go('dashboard');
 	}
 });
--- landing/lib/routes.js
+++ landing/lib/routes.js
@@ -1,10 +1,5 @@
 FlowRouter.route('/', {
   action: function() {
-  	var currentUser = Meteor.users.findOne(Meteor.userId());
-  	if(currentUser) {
-  		BlazeLayout.render('dashboard');
-  	} else {
-  		BlazeLayout.render('landing');
-  	}
+		BlazeLayout.render('layout');
   }
 });
