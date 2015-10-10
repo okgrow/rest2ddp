ef4132cea60774f858ee80a30ce5d0aeb4364df3
Paul Dowman <paul@pauldowman.com>
2015-10-10 16:35:04 -0400

    Fix accidental change to collection name

--- testclient/main.js
+++ testclient/main.js
@@ -1,10 +1,10 @@
 // we'll throw this away, it's just some hardcoded stuff for debugging
 
 if (Meteor.isClient) {
-  testCollection = new Mongo.Collection("test");
+  testCollection = new Mongo.Collection("testCollection");
   Meteor.subscribe("rest2ddp", "test");
   
   Tracker.autorun(function () {
-    console.log(testCollection.find({}, {sort: {_id: 1}}).fetch())
-  })
+    console.log(testCollection.find({}, {sort: {_id: 1}}).fetch());
+  });
 }
