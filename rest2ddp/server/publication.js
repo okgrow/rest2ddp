9770768fa4d3d60ccbdec95da243721cd3967933
Paul Dowman <paul@pauldowman.com>
2015-10-10 15:48:05 -0400

    Use maps to do each change only once

--- rest2ddp/server/publication.js
+++ rest2ddp/server/publication.js
@@ -16,40 +16,61 @@ Meteor.publish("rest2ddp", function () {
   Meteor.setInterval(() => {
     // stringify and parse so that we're sure to have a deep copy
     var result = JSON.parse(JSON.stringify(data));
-    console.log('@@@', "result", result);
+    // console.log('@@@', "result", result);
 
     var diff = DeepDiff.diff(lastResult, result);
-    console.log('@@@', "diff", diff);
+    
+    var added = new Map();
+    var removed = new Map();
+    var changed = new Map();
 
     if (!lastResult) {
       // this is the first time, all are new
       for (var i = 0; i < result.length; i++) {
-        console.log("@@@ Rule 0: added", i, result[i]);
+        // console.log("@@@ Rule 0: added", i, result[i]);
         self.added(config.collectionName, i, result[i]);
       }
     } else if (!diff) {
-      console.log ("@@@ No difference");
+      // console.log ("No difference");
     } else {
+      // console.log('@@@', "diff", diff);
       for (var diffItem of diff) {
         if (diffItem.kind === "A" && diffItem.index && diffItem.path === undefined) {
           if (diffItem.item.kind === "D") {
-            console.log("@@@ Rule 1.1", "removed", diffItem.index);
+            // console.log("@@@ Rule 1.1", "removed", diffItem.index);
+            removed.set(diffItem.index, true);
           } else if (diffItem.item.kind === "N") {
-            console.log("@@@ Rule 1.2", "added", diffItem.index, result[diffItem.index]);
+            // console.log("@@@ Rule 1.2", "added", diffItem.index, result[diffItem.index]);
+            added.set(diffItem.index, result[diffItem.index]);
           }
         } else if (diffItem.kind === "A" && diffItem.path) {
-          console.log('@@@ Rule 2', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          // console.log('@@@ Rule 2', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          changed.set(diffItem.path[0], result[diffItem.path[0]]);
         } else if (diffItem.kind === "E") {
-          console.log('@@@ Rule 3', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          // console.log('@@@ Rule 3', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          changed.set(diffItem.path[0], result[diffItem.path[0]]);
         } else if (diffItem.kind === "N" && diffItem.path) {
-          console.log('@@@ Rule 4', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          // console.log('@@@ Rule 4', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          changed.set(diffItem.path[0], result[diffItem.path[0]]);
         } else if (diffItem.kind === "D" && diffItem.path) {
-          console.log('@@@ Rule 5', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          // console.log('@@@ Rule 5', "changed", diffItem.path[0], result[diffItem.path[0]]);
+          changed.set(diffItem.path[0], result[diffItem.path[0]]);
         } else {
           console.log('!!! Unhandled change', diffItem);
         }
       }
     }
+    
+      added.forEach((doc, id) => {
+        console.log("added", id, ":", doc);
+      });
+      removed.forEach((doc, id) => {
+        console.log("removed", id, ":", doc);
+      });
+      changed.forEach((doc, id) => {
+        console.log("changed", id, ":", doc);
+      });
+      
     
     lastResult = result;
     self.ready();
