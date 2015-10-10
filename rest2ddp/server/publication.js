a5a51aa92601663f22af93d10760aa753abc1166
Paul Dowman <paul@pauldowman.com>
2015-10-10 15:38:33 -0400

    First pass at diffing results with logging only

--- rest2ddp/server/publication.js
+++ rest2ddp/server/publication.js
@@ -14,16 +14,41 @@ Meteor.publish("rest2ddp", function () {
   var lastResult;
   
   Meteor.setInterval(() => {
+    // stringify and parse so that we're sure to have a deep copy
     var result = JSON.parse(JSON.stringify(data));
-    
+    console.log('@@@', "result", result);
+
+    var diff = DeepDiff.diff(lastResult, result);
+    console.log('@@@', "diff", diff);
+
     if (!lastResult) {
       // this is the first time, all are new
       for (var i = 0; i < result.length; i++) {
         console.log("@@@ Rule 0: added", i, result[i]);
         self.added(config.collectionName, i, result[i]);
       }
+    } else if (!diff) {
+      console.log ("@@@ No difference");
     } else {
-      
+      for (var diffItem of diff) {
+        if (diffItem.kind === "A" && diffItem.index && diffItem.path === undefined) {
+          if (diffItem.item.kind === "D") {
+            console.log("@@@ Rule 1.1", "removed", diffItem.index);
+          } else if (diffItem.item.kind === "N") {
+            console.log("@@@ Rule 1.2", "added", diffItem.index, result[diffItem.index]);
+          }
+        } else if (diffItem.kind === "A" && diffItem.path) {
+          console.log('@@@ Rule 2', "changed", diffItem.path[0], result[diffItem.path[0]]);
+        } else if (diffItem.kind === "E") {
+          console.log('@@@ Rule 3', "changed", diffItem.path[0], result[diffItem.path[0]]);
+        } else if (diffItem.kind === "N" && diffItem.path) {
+          console.log('@@@ Rule 4', "changed", diffItem.path[0], result[diffItem.path[0]]);
+        } else if (diffItem.kind === "D" && diffItem.path) {
+          console.log('@@@ Rule 5', "changed", diffItem.path[0], result[diffItem.path[0]]);
+        } else {
+          console.log('!!! Unhandled change', diffItem);
+        }
+      }
     }
     
     lastResult = result;
